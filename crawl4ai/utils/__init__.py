4f5dde05bc3fa9516070336472ce405b833ca6e5
Sebas <sebastian.v.a@riseup.net>
2025-04-15 16:08:13 +0200

    Add utility module documentation and import essential utility functions for Crawl4AI framework

--- crawl4ai/utils/__init__.py
+++ crawl4ai/utils/__init__.py
@@ -1,1 +1,18 @@
-# Crawl4AI module
+"""Utility modules for Crawl4AI framework.
+
+This package provides various utility functions for the framework.
+"""
+
+from .headers import (
+    UserAgentManager,
+    create_headers,
+    COMMON_USER_AGENTS,
+    DEFAULT_HEADERS,
+)
+
+__all__ = [
+    'UserAgentManager',
+    'create_headers',
+    'COMMON_USER_AGENTS',
+    'DEFAULT_HEADERS',
+]
